12cbae49c1dc25645db8d6f33f6f72bf96ebd957
Kashane Sakhakorn Laptop <ksakhakorn@gmail.com>
2022-05-05 19:27:21 -0700

    more controller work and route work

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,7 +18,61 @@ BONUS: Remove a user's associated thoughts when deleted.
 module.exports = {
   getUsers(req, res) {
     User.find()
-      .then(res.json(users))
+      .then(async (users) => {
+        return res.json(users);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      })
+  },
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+    .then('-__v')
+    .then(res.json(res)) // not sure about this res.json within the .then()
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json(err);
+    })
+  },
+  createUser(req, res) {
+    User.create(req)
+      .then() // still need to fill this .then
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      })
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(req)
+      .then() // still need to fill this .then
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      })
+  },
+  deleteUser(req, res) {
+    User.deleteOne(req.params.id)
+      .then( // bonus, trying to delete thoughts created by the deleted user
+        Thought.deleteMany(User.find(req.params.id).thoughts) // definitely not sure if this works
+      )
+      .then(console.log(`Successfully deleted user with id: ${req.params.id}`)) // not sure about this 
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      })
+  },
+  addFriend(req, res) {
+    User.updateOne(req.userId, req.friendId)
+      .then() // still need to fill this .then
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      })
+  },
+  removeFriend(req, res) {
+    User.updateOne(req.userId, req.friendId)
+      .then() // still need to fill this .then
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
 const {
   getUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers);
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
+
+// /api/users/:id
+router.route('/:id')
+  .get(getOneUser)
+  .put(updateUser)
+  .delete(deleteUser)
+
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend)
